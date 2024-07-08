99b0bc124b1ac6019372013ba5770d067bc0d0b5
nitinkumarpals <nitinpal900@gmail.com>
2024-07-08 16:59:28 +0530

    added verification code

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -7,7 +7,54 @@ const UsernameQuerySchema = z.object({
     username: userNameValidation
 });
 
-export async function POST (request: Request) {
+export async function GET (request: Request) {
+
     await dbConnect();
 
+    try {
+        
+        const { searchParams} = new URL(request.url);
+        const queryParams = {
+            username: searchParams.get('username'),
+        };
+
+        const result = UsernameQuerySchema.safeParse(queryParams);
+        console.log(result); //todo remove console
+        if(!result.success){
+            const usernameErrors = result.error.format()._errors || [];
+            return Response.json({
+                success: false,
+                message: usernameErrors?.length > 0 ? usernameErrors.join(', ') : 
+                'invalid query parameters',
+            },
+            { status: 400  }
+        )
+        }
+
+        const { username } = result.data;
+
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true });
+
+        if(existingVerifiedUser){
+            return Response.json({
+                success: false,
+                message: 'username already exists',
+            },{status: 400});
+        }
+
+        return Response.json({ success: true, message: 'username available' },{status: 200});
+
+
+
+    } catch (error) {
+        console.log('error checking username', error);
+        return Response.json({ success: false, message: 'Error checking username' }, { status: 500 });
+    }
+
 }
+export async function POST(response: Response) {
+    return Response.json(
+      { success: false, message: 'POST method is not allowed on this endpoint' },
+      { status: 405 }
+    );
+  }
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,46 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+
+export async function POST(request: Request) {
+
+    await dbConnect();
+    try {
+
+        const { username, code } = await request.json();
+        const decodeUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({ username: decodeUsername });
+
+        if (!user) {
+            return Response.json({ success: false, message: 'user not found' }, { status: 400 });
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = Date.now() < user.verifyCodeExpiry.getTime();
+        if (!user.isVerified && isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+            return Response.json({ success: true, message: 'user verified successfully' }, { status: 200 });
+        }
+        else if (!isCodeNotExpired) {
+            return Response.json({ success: false, message: 'code expired please signup again to get a new code' }, { status: 400 });
+        }
+        else if (!isCodeValid) {
+            return Response.json({ success: false, message: 'code invalid' }, { status: 400 });
+        }
+        else {
+            return Response.json({ success: false, message: 'user is already verified' }, { status: 400 });
+        }
+
+    } catch (error) {
+        console.log('error verifying user', error);
+        return Response.json({ success: false, message: 'Error verifying user' }, { status: 500 });
+    }
+}
+
+export async function GET(response: Response) {
+    return Response.json(
+        { success: false, message: 'GET method is not allowed on this endpoint' },
+        { status: 405 }
+    );
+}
