7629f33800095339ce59a7fe7158c4df87fd497a
nitinkumarpals <nitinpal900@gmail.com>
2024-07-26 17:10:20 +0530

    added ai suggest message in u page

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { CardHeader, CardContent, Card } from '@/components/ui/card';
 import { messageSchema } from '@/schemas/messageSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useParams } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { Textarea } from '@/components/ui/textarea';
 import * as z from 'zod';
@@ -12,18 +13,23 @@ import { Loader2 } from 'lucide-react';
 import axios, { AxiosError } from 'axios';
 import { toast } from '@/components/ui/use-toast';
 import { ApiResponse } from '@/types/ApiResponse';
+import { useCompletion } from 'ai/react'
+import { Separator } from '@/components/ui/separator';
+import { Link } from '@react-email/components';
 
-
+const parseMessagesString = (messageString: string): string[] => {
+  return messageString.split('||');
+}
+const initialMessagesString = "What's your favorite movie?||Which city have you traveled to so far?||What do you want to achieve in life?";
 const page = () => {
-  const [isLoading, setIsLoading] = useState(false);
 
   const params = useParams<{ username: string }>();
   const username = params.username;
   const form = useForm<z.infer<typeof messageSchema>>({
     resolver: zodResolver(messageSchema),
   })
-  const { register, watch, setValue, reset, getValues } = form;
-  const messageContent = watch('content');
+  //suggest message starts
+  const [isLoading, setIsLoading] = useState(false);
   const onSubmit = async (data: z.infer<typeof messageSchema>) => {
     setIsLoading(true);
     try {
@@ -51,13 +57,49 @@ const page = () => {
       setIsLoading(false);
     }
   }
+
+  const { complete, completion, isLoading: isSuggestLoading, error } = useCompletion({
+    api: '/api/suggest-messages',
+    initialCompletion: initialMessagesString
+  });
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: 'Error',
+        description: 'Failed to fetch suggestions',
+        variant: 'destructive'
+      });
+    }
+  }, [error]);
+
+  const { register, watch, setValue, reset, getValues, handleSubmit, control } = form;
+  const messageContent = watch('content');
+  const handleMessageClick = (message: string) => {
+    setValue('content', message);
+  }
+
+  const fetchSuggestedMessages = async () => {
+    try {
+      complete('');
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+      toast({
+        title: 'Error',
+        description: 'Failed to fetch messages',
+        variant: 'destructive'
+      });
+    }
+  };
+
   return (
     <div className='min-h-screen relative'>
       <div className='container mx-auto my-8 p-6 max-w-4xl mt-5'>
         <h1 className='text-4xl font-bold text-center mb-6'>Public Profile Link</h1>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <FormField
+              control={control}
               name="content"
               render={({ field }) => (
                 <FormItem>
@@ -71,7 +113,7 @@ const page = () => {
                   </FormControl>
                 </FormItem>
               )} />
-            <div className=' justify-center'></div>
+            <div className='flex justify-center'>
             {isLoading ? (
               <Button disabled>
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
@@ -80,8 +122,49 @@ const page = () => {
             ) : (
               <Button type="submit" disabled={isLoading || !messageContent}>Send</Button>
             )}
+            </div>
           </form>
         </Form>
+
+        <div className='space-y-4 my-8'>
+          <div className='space-y-2'>
+            <Button onClick={fetchSuggestedMessages} disabled={false}>Suggest Messages</Button>
+            <p>Click on any message below to select it.</p>
+          </div>
+          <Card>
+            <CardHeader>
+              <h3 className='text-xl font-semibold'>Messages</h3>
+            </CardHeader>
+            <CardContent className='flex flex-col space-y-4'>
+              {error ? (
+                parseMessagesString(initialMessagesString).map((message, index) => (
+                  <Button
+                    key={index}
+                    variant="outline"
+                    onClick={() => handleMessageClick(message)}>
+                    {message}
+                  </Button>
+                ))
+              ) : (
+                parseMessagesString(completion).map((message, index) => (
+                  <Button
+                    key={index}
+                    variant="outline"
+                    onClick={() => handleMessageClick(message)}>
+                    {message}
+                  </Button>
+                ))
+              )}
+            </CardContent>
+          </Card>
+        </div>
+        <Separator className="my-6" />
+        <div className="text-center">
+        <div className="mb-4">Get Your Message Board</div>
+        <Link href={'/sign-up'}>
+          <Button>Create Your Account</Button>
+        </Link>
+      </div>
       </div>
     </div>
   )
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,8 +1,5 @@
 import { z } from "zod";
 
 export const messageSchema = z.object({
-    content: z
-        .string()
-        .min(10, "Message must be at least 10 character long")
-        .max(300, "Message must be at most 300 characters long")
-})
+    content: z.string().min(10,{ message: 'Message must be at least 10 character long'}).max(300, {message: 'Message must be at most 300 characters long'})
+});
